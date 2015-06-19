43e1c83473184b39ee08b7fcd2f089a4c8ef627c
Dominik Schreiber <domischreib@gmail.com>
2015-06-19 08:57:20 +0200

    added call to engine.getPlayers()

--- directives/players/playersDirective.js
+++ directives/players/playersDirective.js
@@ -1,14 +1,22 @@
-angular.module('blackjack.playersDirective', ['blackjack.playerDirective', 'blackjack.engineService', 'ng'])
+angular.module('blackjack.playersDirective', [
+    'blackjack.playerDirective',
+    'blackjack.engineService',
+    'ng'
+])
 
 .directive('players', function(engine) {
     return {
         templateUrl: 'directives/players/playersDirective.tpl.html',
         link: function($scope, $element, $attributes) {
-            $scope.players = [];
+            $scope.joined = false;
+
+            engine.getPlayers().then(function(players) {
+                $scope.players = _.map(players, function(player) { return { id: player, human: false } });
+            });
 
             $scope.join = function() {
                 engine.addPlayer().then(function(player) {
-                    $scope.players.push(player);
+                    $scope.players.push({ id: player, human: true });
                     $scope.joined = true;
                 });
             };
