dab816b6712200eb607ef2f8616a73602af7d8ea
Dominik Schreiber <domischreib@gmail.com>
2015-06-22 12:57:30 +0200

    changed card style to have big suit on bottom right corner

--- directives/card/cardDirective.js
+++ directives/card/cardDirective.js
@@ -2,11 +2,17 @@
 
 angular.module('blackjack.cardDirective', ['ng'])
 
-.directive('card', function() {
+.directive('card', function($timeout) {
     return {
         scope: {
             content: '='
         },
-        templateUrl: 'directives/card/cardDirective.tpl.html'
+        templateUrl: 'directives/card/cardDirective.tpl.html',
+        link: function($scope, $element, $attributes) {
+            $scope.isNew = true;
+            $timeout(function() {
+                $scope.isNew = false;
+            }, 1);
+        }
     };
 });
